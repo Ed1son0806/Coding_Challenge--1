2af0b6cbce144b119be106c3fe6dfb74393a94df
Edison <aaedison0806@gmail.com>
2025-01-26 13:59:54 -0500

    Initial Commit for testing

---
+++ cc_1.js
@@ -0,0 +1,26 @@
+let employeeName = "Sven";
+console.log(employeeName);
+const employeeID = 708;
+console.log(employeeID);
+var isAvailable = true;
+console.log(isAvailable);
+let productName = "Diamond"; 
+console.log(productName);
+const productPrice = 1000000;
+console.log(productPrice);
+var isAvailable = true;
+console.log(isAvailable);
+let accountBalance = 3000;
+let deposite = 1000;
+accountBalance += deposite;
+console.log(accountBalance);
+let customerName = "Eric";
+let welcomeMessage = "Welcome, " + customerName;
+console.log(welcomeMessage);
+let isLoggedIn = true;
+let hasPassword = true;
+if (isLoggedIn && hasPassword) {
+    console.log("Log In Successfull!");
+} else if (isLoggedIn && !hasPassword) {
+    console.log("Log In Unsuccessful, incorrect username or password")
+}
